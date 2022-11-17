1c71b598c2adf375de3fedf113ff8935c336c425
Madabattula Sai Yerni Akhil <makhil123@yahoo.in>
2022-11-17 23:11:08 +0530

    Add partytown plugin and google analytics tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,15 @@ import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })],
   output: "server",
   adapter: netlify()
 });
