88d32ef252edf319a7cc43f4cf51950b21e8bb5b
Sai Yerni Akhil Madabattula <makhil123@yahoo.in>
2024-04-28 22:36:32 -0500

    upgrade to astro v4 🚀

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
